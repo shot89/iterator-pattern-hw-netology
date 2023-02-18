9f07999784d1d5fd30d3f7cdd8bdf66ca779b8c4
Shot <shot89@gmail.com>
2023-02-18 11:35:57 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,34 @@
+package ru.netology;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+
+    protected Random random = new Random();
+    private int min, max;
+
+    public Randoms(int min, int max) {
+        this.min = min;
+        this.max = max;
+        iterator();
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RandomsIterator();
+    }
+
+
+    private class RandomsIterator implements Iterator<Integer> {
+        @Override
+        public boolean hasNext() {
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            return random.nextInt(max - min + 1) + min;
+        }
+    }
+}
