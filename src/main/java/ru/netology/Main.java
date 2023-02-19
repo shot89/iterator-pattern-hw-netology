407bd0b201a5b3851bb9b56e2f7a880ffd72a2f3
Shot <shot89@gmail.com>
2023-02-19 18:58:43 +0300

    new version

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,9 +1,10 @@
 package ru.netology;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for (int r : new Randoms(90, 100)) {
             System.out.println("Случайное число: " + r);
+            Thread.sleep(1000);
             if (r == 100) {
                 System.out.println("Выпало число 100, давайте на этом закончим");
                 break;
